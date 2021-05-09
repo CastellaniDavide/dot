fba0406d207c15ecd16aa4cee13f448b927cdde2
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-05-09 17:45:52 +0200

    v01.01

--- bin/dot.py
+++ bin/dot.py
@@ -1,19 +1,76 @@
 """dot
 """
+import asyncio
+import discord
+from dotenv import load_dotenv
+from os import getenv
 
-__author__ = "help@castellanidavide.it"
-__version__ = "1.0 2021-5-9"
+__author__ = "Castellani Davide", "De Battisti Tommaso"
+__version__ = "01.01 2021-05-09"
 
-class dot:
-	def __init__ (self):
-		"""Where it all begins
-		"""
-		print(dot.dot())
-	
-	def dot():
-		"""dot first funtion
-		"""
-		return "dot" #for the test (see test_dot.py)
-		
-if __name__ == "__main__":
-	dot()
+class dot(discord.Client):
+    def __init__(self):
+        """ Constructor
+        """
+        super().__init__()
+        self.online = {}
+        self.locks = {}
+
+    def get_lock(self, server, channel):
+        """ Gets the lock
+        """
+        try:
+            return self.locks[server][channel]
+        except KeyError:
+            self.locks[server] = {}
+            self.locks[server][channel] = asyncio.Lock()
+        
+        return self.locks[server][channel]
+
+    def set_online(self, server, channel, value):
+        """ Set online to the given value
+        """
+        try:
+            self.online[server][channel] = value
+        except KeyError:
+            self.online[server] = {}
+
+        self.online[server][channel] = value
+
+    def get_online(self, server, channel):
+        """ Gets the online state
+        """
+        try:
+            return self.online[server][channel]
+        except KeyError:
+            return False
+
+    async def on_ready(self):
+        """ Print a message when online
+        """
+        print(f"{self.user} has connected to Discord")
+
+    async def on_message(self, message):
+        """ Elaboration on each message
+        """
+        message_string = str(message.content)
+        message_guild = str(message.guild.name)
+        message_channel = str(message.channel.name)
+        message_author = str(message.author.name)
+
+        async with self.get_lock(message_guild, message_channel):
+            if message_string == "." and message_author != self.user.display_name:
+                self.set_online(message_guild, message_channel, True)
+            if message_string.lower() == "carambola":
+                self.set_online(message_guild, message_channel, False)
+
+            await asyncio.sleep(1)
+
+            online = self.get_online(message_guild, message_channel)
+            if online:
+                await message.reply(".")
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    dot().run(getenv("DISCORD_TOKEN"))
--- docs/README.md
+++ docs/README.md
@@ -1,27 +1,23 @@
 # dot
-[![GitHub license](https://img.shields.io/badge/license-GNU-green?style=flat)](https://github.com/CastellaniDavide/cpp-dot/blob/master/LICENSE) ![Author](https://img.shields.io/badge/author-Castellani%20Davide-green?style=flat) ![Version](https://img.shields.io/badge/version-v1.0-blue?style=flat) ![Language Python](https://img.shields.io/badge/language-Python-yellowgreen?style=flat) ![sys.platform supported](https://img.shields.io/badge/OS%20platform%20supported-Linux,%20Windows%20&%20Mac%20OS-blue?style=flat) [![On GitHub](https://img.shields.io/badge/on%20GitHub-True-green?style=flat&logo=github)](https://github.com/CastellaniDavide/dot)
+[![GitHub license](https://img.shields.io/badge/license-GNU-green?style=flat)](https://github.com/CastellaniDavide/cpp-dot/blob/master/LICENSE) ![Author](https://img.shields.io/badge/author-Castellani%20Davide-green?style=flat) ![Author](https://img.shields.io/badge/author-De%20Battisti%20Tommaso-green?style=flat) ![Version](https://img.shields.io/badge/version-v01.01-blue?style=flat) ![Language Python](https://img.shields.io/badge/language-Python-yellowgreen?style=flat) ![sys.platform supported](https://img.shields.io/badge/OS%20platform%20supported-Linux,%20Windows%20&%20Mac%20OS-blue?style=flat) [![On GitHub](https://img.shields.io/badge/on%20GitHub-True-green?style=flat&logo=github)](https://github.com/CastellaniDavide/dot)
 
 ## Description
 Ruin your Discord server with this bot
 
 ## Required
  - python3
+- pip3 librerias (in project core: ```pip3 install -r requirements/requirements.txt```)
  
 ## Directories structure
  - .github
    - ISSUE_TEMPLATE
      - bug_report.md
      - feature-request.md
-   - workflows
-     - python-test.yml
  - bin
    - dot.py
-   - test_dot.py
  - docs
    - LICENSE
    - README.md
- - log
-   - trace.log
  - requirements
    - requirements.txt
    
@@ -30,13 +26,12 @@ Ruin your Discord server with this bot
  - python3 test_dot.py
 
 # Changelog
- - [Version_1.0_2021-5-9](#Version_10_2021-5-9)
+ - [Version_01.01_2021-05-09](#Version_0101_2021-05-09)
 
-## Version_1.0_2021-5-9
+## Version_01.01_2021-05-09
  - Initial version
 
 ---
-Made by Castellani Davide 
-If you have any problem please contact me:
-- help@castellanidavide.it
+Made by Castellani Davide & De Battisti Tommaso 
+If you have any problem please contact us:
 - [Issue](https://github.com/CastellaniDavide/dot/issues)
