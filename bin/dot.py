a2b9220a219aa1db1e8aeac57111f7493259ba66
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-05-09 17:24:19 +0200

    Created bin/dot.py

---
+++ bin/dot.py
@@ -0,0 +1,19 @@
+"""dot
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-5-9"
+
+class dot:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(dot.dot())
+	
+	def dot():
+		"""dot first funtion
+		"""
+		return "dot" #for the test (see test_dot.py)
+		
+if __name__ == "__main__":
+	dot()
